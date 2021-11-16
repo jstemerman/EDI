76e3dc660c48e341c4317f764633c0b08845b332
jstemerman <jackstemerman@gmail.com>
2021-11-16 14:07:51 -0500

    Changing background color

---
+++ CreativeCoding/p5/BackgroundColor/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas (windowWidth, windowHeight);//This creates the window width and height to be the size of my browser
+background (255, 255, 0)//red background
+}
+
+function draw() {
+
+}
