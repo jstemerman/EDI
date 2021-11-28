c4623f32140e2ee3ef6ea151601b56210395a11f
jstemerman <jackstemerman@gmail.com>
2021-11-28 17:38:16 -0500

    2x2 loop

---
+++ CreativeCoding/p5/2x2_Loop/sketch.js
@@ -0,0 +1,22 @@
+function setup() {//runs once
+  createCanvas(720, 720);
+  fill ('magenta');
+  strokeWeight (8);
+}
+
+function draw() {//runs in a loop
+  background(220);
+  
+  translate(60,60);
+  
+  for(var y = 0; y < 600; y = y + 300)//loop to create columns 
+      
+  for(var x = 0; x < 600; x = x + 300) {//loop to create rows
+    quad (x, y,
+       x + 300, y,
+       x + 300, y + 300,
+       x, y + 300);
+ }
+
+  noLoop()
+}
