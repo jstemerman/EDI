d4123c603043f4e2ec7b7f84ac4ab608d04c1635
jstemerman <jackstemerman@gmail.com>
2021-12-02 09:27:52 -0500

    Update sketch.js

--- CreativeCoding/p5/Tuning_with_Variables/sketch.js
+++ CreativeCoding/p5/Tuning_with_Variables/sketch.js
@@ -5,14 +5,14 @@ function setup() {//runs once
 }
 
 function draw() {//runs in a loop
-  
+
 
   var num = 2; // variable for the number of squares in the x direction
-  
+
     var sideLen = windowWidth/num; //variable for the side length of each square
-  
-  for(var y = 0; y < windowWidth; y = y + sideLen) {//loop to create columns 
-      
+
+  for(var y = 0; y < windowWidth; y = y + sideLen) {//loop to create columns
+
   for(var x = 0; x < windowWidth; x = x + sideLen){ //loop to create rows
     quad (x, y,
        x + sideLen, y,
