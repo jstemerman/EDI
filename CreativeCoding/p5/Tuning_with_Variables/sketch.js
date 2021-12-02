c09f58bec29c961d719911e3c39a7ae0241b943d
jstemerman <jackstemerman@gmail.com>
2021-12-02 09:22:16 -0500

    fixed tuning

--- CreativeCoding/p5/Tuning_with_Variables/sketch.js
+++ CreativeCoding/p5/Tuning_with_Variables/sketch.js
@@ -1,6 +1,6 @@
 function setup() {//runs once
   createCanvas(windowWidth, windowWidth);
-  fill ('magenta');
+  fill ('red');
   strokeWeight (1);
 }
 
@@ -11,14 +11,15 @@ function draw() {//runs in a loop
   
     var sideLen = windowWidth/num; //variable for the side length of each square
   
-  for(var y = 0; y < windowWidth; y = sideLen)//loop to create columns 
+  for(var y = 0; y < windowWidth; y = y + sideLen) {//loop to create columns 
       
-  for(var x = 0; x < windowWidth; x = sideLen) {//loop to create rows
+  for(var x = 0; x < windowWidth; x = x + sideLen){ //loop to create rows
     quad (x, y,
        x + sideLen, y,
        x + sideLen, y + sideLen,
        x, y + sideLen);
  }
+  }
 
   noLoop()
 }
