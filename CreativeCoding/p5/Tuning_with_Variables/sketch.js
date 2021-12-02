8183486df66cb61e7642f530bfc9e228399c0c5a
jstemerman <jackstemerman@gmail.com>
2021-12-02 10:19:54 -0500

    making gif and putting on website

--- CreativeCoding/p5/Tuning_with_Variables/sketch.js
+++ CreativeCoding/p5/Tuning_with_Variables/sketch.js
@@ -1,25 +1,30 @@
-function setup() {//runs once
-  createCanvas(windowWidth, windowHeight);
-  fill ('red');
-  strokeWeight (1);
+let gif;
+
+function preload (){
+  gif = loadImage ('Assets2/Skull.gif');
 }
 
-function draw() {//runs in a loop
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('indigo');
+  strokeWeight(1);
+}
 
+function draw() {
 
-  var num = 6; // variable for the number of squares in the x direction
+var num = 10; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
 
-    var sideLen = windowWidth/num; //variable for the side length of each square
-
-  for(var y = 0; y < windowHeight; y = y + sideLen) {//loop to create columns
-
-  for(var x = 0; x < windowWidth; x = x + sideLen){ //loop to create rows
-    quad (x, y,
-       x + sideLen, y,
-       x + sideLen, y + sideLen,
-       x, y + sideLen);
- }
+  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+image (gif, x, y);
   }
+}
 
-  noLoop()
+//noLoop ();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
