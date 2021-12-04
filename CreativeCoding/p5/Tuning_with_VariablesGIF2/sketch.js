da999414530bfe183f2d93fc7f69f0b93cc22f9c
jstemerman <jackstemerman@gmail.com>
2021-12-03 21:44:12 -0500

    coding again

--- CreativeCoding/p5/Tuning_with_VariablesGIF2/sketch.js
+++ CreativeCoding/p5/Tuning_with_VariablesGIF2/sketch.js
@@ -15,12 +15,13 @@ background (255);
 
 translate(-100, -100)
 
-var num = 50; //variable for the number of squares in the array
+var num = 65; //variable for the number of squares in the array
 var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
      for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
     image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
+
 
   }
 }
