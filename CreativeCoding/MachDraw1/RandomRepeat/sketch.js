fc8ce40fa740938a0a1964c7ef49dba0b7ebf958
jstemerman <jackstemerman@gmail.com>
2022-01-11 11:21:15 -0500

    Update sketch.js

--- CreativeCoding/MachDraw1/RandomRepeat/sketch.js
+++ CreativeCoding/MachDraw1/RandomRepeat/sketch.js
@@ -25,15 +25,8 @@ function draw() {
     line (x + 10, y, x + 10 + random (35, 50), y + random(40, 50));
     line (x + 50, y + 50, x + 50 - random(30, 50), y + 50 + random (35,50));
     line (x + 40, y + 50, x + 40 - random(30, 50), y + 50 + random (35,50));
-  }
-}
-
-function draw() {
-    for (var y = 0; y < canvasHeight; y = y + 100){
-    for (var x = 50; x < canvasWidth; x = x + 100){
-      ellipse(x - 10, y + 25, random(8, 23), random ( 8, 23));
-      ellipse(x + 60, y + 75, random(8, 23), random(8, 23));
-      rotate(PI/3.0)
+    ellipse(x - 10, y + 25, random(8, 23), random ( 8, 23));
+    ellipse(x + 60, y + 75, random(8, 23), random(8, 23));
     //ellipse(x + 55, y + 25 + random(-5, 5), random(6, 13), random ( 9, 16));
     //ellipse(x - 5, y + 75 + random(-5, 5), random(6, 13), random ( 9, 16))
 
