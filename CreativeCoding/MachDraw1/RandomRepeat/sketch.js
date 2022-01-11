a1fec4419f2498286a560ae989a82fcbce9269d6
jstemerman <jackstemerman@gmail.com>
2022-01-11 11:18:04 -0500

    Update sketch.js

--- CreativeCoding/MachDraw1/RandomRepeat/sketch.js
+++ CreativeCoding/MachDraw1/RandomRepeat/sketch.js
@@ -12,7 +12,7 @@ var canvasHeight = pH * 72
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight, SVG); // Create SVG Canvas
-  strokeWeight(2); // do 0.1 for laser
+  strokeWeight(1); // do 0.1 for laser
   stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
   background(230)
@@ -25,8 +25,15 @@ function draw() {
     line (x + 10, y, x + 10 + random (35, 50), y + random(40, 50));
     line (x + 50, y + 50, x + 50 - random(30, 50), y + 50 + random (35,50));
     line (x + 40, y + 50, x + 40 - random(30, 50), y + 50 + random (35,50));
-    ellipse(x - 10 + random(-2, 3), y + 25 + random(-5, 5), random(16, 23), random ( 8, 12));
-    ellipse(x + 60 + random (-2, 3), y + 75 + random(-5, 5), random(16, 23), random(8, 12));
+  }
+}
+
+function draw() {
+    for (var y = 0; y < canvasHeight; y = y + 100){
+    for (var x = 50; x < canvasWidth; x = x + 100){
+      ellipse(x - 10, y + 25, random(8, 23), random ( 8, 23));
+      ellipse(x + 60, y + 75, random(8, 23), random(8, 23));
+      rotate(PI/3.0)
     //ellipse(x + 55, y + 25 + random(-5, 5), random(6, 13), random ( 9, 16));
     //ellipse(x - 5, y + 75 + random(-5, 5), random(6, 13), random ( 9, 16))
 
