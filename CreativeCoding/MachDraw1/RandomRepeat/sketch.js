8f2576f06ba576a8e606d4278e7528205675a78b
jstemerman <jackstemerman@gmail.com>
2022-01-12 21:06:05 -0500

    finished Coding

--- CreativeCoding/MachDraw1/RandomRepeat/sketch.js
+++ CreativeCoding/MachDraw1/RandomRepeat/sketch.js
@@ -22,11 +22,11 @@ function draw() {
   for (var y = 0; y < canvasHeight; y = y + 100){
   for (var x = 50; x < canvasWidth; x = x + 100){
     line (x, y, x + random (30, 50), y + random (35, 50));
-    line (x + 10, y, x + 10 + random (35, 50), y + random(40, 50));
+    line (x + 10, y, x + 10 + random (25, 50), y + random(40, 50));
     line (x + 50, y + 50, x + 50 - random(30, 50), y + 50 + random (35,50));
-    line (x + 40, y + 50, x + 40 - random(30, 50), y + 50 + random (35,50));
-    ellipse(x - 10, y + 25, random(8, 23), random ( 8, 23));
-    ellipse(x + 60, y + 75, random(8, 23), random(8, 23));
+    line (x + 40, y + 50, x + 40 - random(25, 50), y + 50 + random (40,50));
+    ellipse(x - 10.5, y + 25, random(8, 23), random ( 8, 19));
+    ellipse(x + 60.5, y + 75, random(8, 23), random(8, 19));
     //ellipse(x + 55, y + 25 + random(-5, 5), random(6, 13), random ( 9, 16));
     //ellipse(x - 5, y + 75 + random(-5, 5), random(6, 13), random ( 9, 16))
 
@@ -34,7 +34,7 @@ function draw() {
 }
 
 
-  //save("mySVG.svg"); // give file name
-  //print("saved svg");
+  save("mySVG.svg"); // give file name
+  print("saved svg");
   noLoop(); // we just want to export once
 }
